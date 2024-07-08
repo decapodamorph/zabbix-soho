bb8c2ee476ea4a5c059b65ce985955f5b9651ad7
decapodamorph <113974750+decapodamorph@users.noreply.github.com>
2024-07-08 15:43:28 +0200

    edited to use specific versions of images, edited entrypoint for server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,125 @@
 #!/bin/bash
-set -e
 
-# Perform initial configuration or setup here
-echo "Starting Zabbix Server setup..."
+set -o pipefail
 
-# Check database connection
-DB_HOST="${DB_SERVER_HOST:-localhost}"
-DB_USER="${MYSQL_USER}"
-DB_PASS="${MYSQL_PASSWORD}"
-DB_NAME="${MYSQL_DATABASE}"
+set +e
 
-echo "Checking database connection to $DB_HOST"
-until mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" -e "SHOW DATABASES;" ; do
-  >&2 echo "Database is unavailable - sleeping"
-  sleep 3
-done
+# Script trace mode
+if [ "${DEBUG_MODE,,}" == "true" ]; then
+    set -o xtrace
+fi
 
->&2 echo "Database is up - executing command"
+# Default directories
+# User 'zabbix' home directory
+ZABBIX_USER_HOME_DIR="/var/lib/zabbix"
+# Configuration files directory
+ZABBIX_ETC_DIR="/etc/zabbix"
 
-# Optionally configure database, migrate schemas, etc.
-# ...
+escape_spec_char() {
+    local var_value=$1
 
-# Start the main application (e.g., Zabbix Server)
-/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
+    var_value="${var_value//\\/\\\\}"
+    var_value="${var_value//[$'\n']/}"
+    var_value="${var_value//\//\\/}"
+    var_value="${var_value//./\\.}"
+    var_value="${var_value//\*/\\*}"
+    var_value="${var_value//^/\\^}"
+    var_value="${var_value//\$/\\\$}"
+    var_value="${var_value//\&/\\\&}"
+    var_value="${var_value//\[/\\[}"
+    var_value="${var_value//\]/\\]}"
 
-# Exec passed arguments (allows for running container with custom commands)
+    echo "$var_value"
+}
+
+update_config_var() {
+    local config_path=$1
+    local var_name=$2
+    local var_value=$3
+    local is_multiple=$4
+
+    if [ ! -f "$config_path" ]; then
+        echo "**** Configuration file '$config_path' does not exist"
+        return
+    fi
+
+    echo -n "** Updating '$config_path' parameter \"$var_name\": '$var_value'..."
+
+    # Remove configuration parameter definition in case of unset parameter value
+    if [ -z "$var_value" ]; then
+        sed -i -e "/^$var_name=/d" "$config_path"
+        echo "removed"
+        return
+    fi
+
+    # Remove value from configuration parameter in case of double quoted parameter value
+    if [ "$var_value" == '""' ]; then
+        sed -i -e "/^$var_name=/s/=.*/=/" "$config_path"
+        echo "undefined"
+        return
+    fi
+
+    # Use full path to a file for TLS related configuration parameters
+    if [[ $var_name =~ ^TLS.*File$ ]]; then
+        var_value=$ZABBIX_USER_HOME_DIR/enc/$var_value
+    fi
+
+    # Escaping characters in parameter value and name
+    var_value=$(escape_spec_char "$var_value")
+    var_name=$(escape_spec_char "$var_name")
+
+    if [ "$(grep -E "^$var_name=" $config_path)" ] && [ "$is_multiple" != "true" ]; then
+        sed -i -e "/^$var_name=/s/=.*/=$var_value/" "$config_path"
+        echo "updated"
+    elif [ "$(grep -Ec "^# $var_name=" $config_path)" -gt 1 ]; then
+        sed -i -e  "/^[#;] $var_name=$/i\\$var_name=$var_value" "$config_path"
+        echo "added first occurrence"
+    elif [ "$(grep -Ec "^[#;] $var_name=" $config_path)" -gt 0 ]; then
+        sed -i -e "/^[#;] $var_name=/s/.*/&\n$var_name=$var_value/" "$config_path"
+        echo "added"
+    else
+        sed -i -e '$a\' -e "$var_name=$var_value" "$config_path"
+        echo "added at the end"
+    fi
+
+}
+
+prepare_zbx_web_service_config() {
+    echo "** Preparing Zabbix web service configuration file"
+    ZBX_CONFIG=$ZABBIX_ETC_DIR/zabbix_web_service.conf
+
+    update_config_var $ZBX_CONFIG "LogType" "console"
+    update_config_var $ZBX_CONFIG "LogFile"
+    update_config_var $ZBX_CONFIG "LogFileSize"
+    update_config_var $ZBX_CONFIG "DebugLevel" "${ZBX_DEBUGLEVEL}"
+
+    : ${ZBX_ALLOWEDIP:="zabbix-server"}
+    update_config_var $ZBX_CONFIG "AllowedIP" "${ZBX_ALLOWEDIP}"
+
+    update_config_var $ZBX_CONFIG "ListenPort" "${ZBX_LISTENPORT}"
+
+    update_config_var $ZBX_CONFIG "Timeout" "${ZBX_TIMEOUT}"
+
+    update_config_var $ZBX_CONFIG "TLSAccept" "${ZBX_TLSACCEPT}"
+    update_config_var $ZBX_CONFIG "TLSCAFile" "${ZBX_TLSCAFILE}"
+
+    update_config_var $ZBX_CONFIG "TLSCertFile" "${ZBX_TLSCERTFILE}"
+    update_config_var $ZBX_CONFIG "TLSKeyFile" "${ZBX_TLSKEYFILE}"
+
+    update_config_var $ZBX_CONFIG "IgnoreURLCertErrors" "${ZBX_IGNOREURLCERTERRORS}"
+}
+
+prepare_web_service() {
+    echo "** Preparing Zabbix web service"
+    prepare_zbx_web_service_config
+}
+
+#################################################
+
+if [ "$1" == '/usr/sbin/zabbix_web_service' ]; then
+    prepare_web_service
+fi
+
 exec "$@"
+
+#################################################
--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 # Prompt for MySQL credentials and hide input for passwords
 read -sp "Enter MYSQL_ROOT_PASSWORD: " MYSQL_ROOT_PASSWORD
 echo
-read -p "Enter MYSQL_DATABASE: " MYSQL_DATABASE
-read -p "Enter MYSQL_USER: " MYSQL_USER
 read -sp "Enter MYSQL_PASSWORD: " MYSQL_PASSWORD
 echo
 
@@ -15,8 +13,8 @@ echo
 # Write to .env file
 cat <<EOL > .env
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
-MYSQL_DATABASE=${MYSQL_DATABASE}
-MYSQL_USER=${MYSQL_USER}
+MYSQL_DATABASE=zabbix
+MYSQL_USER=zabbix
 MYSQL_PASSWORD=${MYSQL_PASSWORD}
 ZABBIX_PASSWORD=${ZABBIX_PASSWORD}
 EOL
